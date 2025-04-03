4e8a69e5da1206aefd5c3efe4e913b5212fdbf63
Tamal Anwar Chowdhury <tamalanwaronline@gmail.com>
2025-04-03 21:49:12 +0600

    feat: lifetime_subscriber

--- kindeSrc/environment/workflows/supabaseWorkflow.ts
+++ kindeSrc/environment/workflows/supabaseWorkflow.ts
@@ -23,9 +23,26 @@ export const workflowSettings: WorkflowSettings = {
 export default async function SupabaseWorkflow(
   event: onUserTokenGeneratedEvent
 ) {
+  const SUPABASE_ANON_KEY = getEnvironmentVariable("SUPABASE_ANON_KEY")?.value;
+  const SUPABASE_URL = getEnvironmentVariable("SUPABASE_URL")?.value;
+
   const accessToken = accessTokenCustomClaims<{
-    myCustomFact: string;
+    lifetime_subscriber: boolean;
   }>();
 
-  accessToken.myCustomFact = "hello";
+  const response = await fetch(
+    `${SUPABASE_URL}/rest/v1/profiles?kinde_id=eq.${event.user.id}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${SUPABASE_ANON_KEY}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  if (response.data.length > 0) {
+    const profile = response.data[0];
+    accessToken.lifetime_subscriber = profile?.lifetime_subscriber;
+  }
 }
